c487d011001c513917cd0012e881552bc3b8662b
ahmadhamzh <ahmadhamzh@ymail.com>
2021-11-09 19:43:50 +0200

    code 08

--- code-chalenges-401/Singly-Linked-Lists/__test__/Linkedlist.test.js
+++ code-chalenges-401/Singly-Linked-Lists/__test__/Linkedlist.test.js
@@ -50,7 +50,7 @@ describe('linkedlist checking', () => {
         expect(ll.includes('mohammed')).toEqual(true)
 
     })
-    
+
     test('Will return false when searching for a value in the linked list that does not exist', () => {
 
         const ll = new Linkedlist()
@@ -60,7 +60,7 @@ describe('linkedlist checking', () => {
         expect(ll.includes('ibrahem')).toEqual(false)
 
     })
-    
+
     test('Can properly return a collection of all the values that exist in the linked list', () => {
 
         const ll = new Linkedlist()
@@ -70,14 +70,14 @@ describe('linkedlist checking', () => {
         expect(ll.toString()).toEqual('{ahmad} -> {mohammed} -> {hamzh} -> {null} ')
 
     })
-    
+
     test('CAN ADD NODE AFTER SPECIFIC NODE', () => {
 
         const ll = new Linkedlist()
         ll.append('ahmad')
         ll.append('mohammed')
         ll.append('hamzh')
-        ll.insertafter('mohammed','ibrahim')
+        ll.insertafter('mohammed', 'ibrahim')
         expect(ll.toString()).toEqual('{ahmad} -> {mohammed} -> {ibrahim} -> {hamzh} -> {null} ')
 
     })
@@ -88,11 +88,11 @@ describe('linkedlist checking', () => {
         ll.append('ahmad')
         ll.append('mohammed')
         ll.append('hamzh')
-        ll.insertBefore('hamzh','ibrahim')
+        ll.insertBefore('hamzh', 'ibrahim')
         expect(ll.toString()).toEqual('{ahmad} -> {mohammed} -> {ibrahim} -> {hamzh} -> {null} ')
 
     })
-    
+
     test('Where k is greater than the length of the linked list', () => {
 
         const ll = new Linkedlist()
@@ -100,11 +100,11 @@ describe('linkedlist checking', () => {
         ll.append('1')
         ll.append('2')
         ll.append('3')
-        
+
         expect(ll.kthFromEnd(6)).toEqual('Exception')
 
     })
-    
+
     test('Where k and the length of the list are the same', () => {
 
         const ll = new Linkedlist()
@@ -112,11 +112,11 @@ describe('linkedlist checking', () => {
         ll.append('1')
         ll.append('2')
         ll.append('3')
-        
+
         expect(ll.kthFromEnd(4)).toEqual('Exception')
 
     })
-    
+
     test('Where k is not a positive integer', () => {
 
         const ll = new Linkedlist()
@@ -124,11 +124,11 @@ describe('linkedlist checking', () => {
         ll.append('1')
         ll.append('2')
         ll.append('3')
-        
+
         expect(ll.kthFromEnd(-3)).toEqual('Exception')
 
     })
-    
+
     test('Where the linked list is of a size 1', () => {
 
         const ll = new Linkedlist()
@@ -136,11 +136,11 @@ describe('linkedlist checking', () => {
         ll.append('1')
         ll.append('2')
         ll.append('3')
-        
+
         expect(ll.kthFromEnd(1)).toEqual('2')
 
     })
-    
+
     test('where k is not at the end, but somewhere in the middle of the linked list', () => {
 
         const ll = new Linkedlist()
@@ -149,13 +149,57 @@ describe('linkedlist checking', () => {
         ll.append('2')
         ll.append('3')
         ll.append('4')
-        
+
         expect(ll.kthFromEnd(2)).toEqual('2')
 
     })
 
+    test('check if the tow list isnt equal', () => {
 
+        const ll1 = new Linkedlist();
+        const ll2 = new Linkedlist();
+        const ZipLL1 = new Linkedlist();
+        ll1.append('1')
+        ll1.append('3')
+        ll1.append('5')
+        ll1.append('7')
+        ll1.append('9')
+        ll1.append('11')
+        ll1.append('13')
+        ll2.append('2')
+        ll2.append('4')
+        ll2.append('6')
+        ll2.append('8')
+        ZipLL1.zipLists(ll1,ll2)
 
+        expect(ZipLL1.toString()).toEqual('{1} -> {2} -> {3} -> {4} -> {5} -> {6} -> {7} -> {8} -> {9} -> {11} -> {13} -> {null} ')
 
+    })
+
+    test('check if one of the list is empty', () => {
+
+        const ll1 = new Linkedlist();
+        const ll2 = new Linkedlist();
+        const ZipLL1 = new Linkedlist();
+        ll1.append('1')
+        ll1.append('3')
+        ll1.append('5')
+        ll1.append('7')               
+        ZipLL1.zipLists(ll1,ll2)
+
+        expect(ZipLL1.zipLists(ll1,ll2)).toEqual(ll1)
+
+    })
+    
+    test('check if both of the list is empty', () => {
+
+        const ll1 = new Linkedlist();
+        const ll2 = new Linkedlist();
+        const ZipLL1 = new Linkedlist();             
+        ZipLL1.zipLists(ll1,ll2)
+
+        expect(ZipLL1.zipLists(ll1,ll2)).toEqual(ll1)
+
+    })
 
 })
--- code-chalenges-401/Singly-Linked-Lists/readme.md
+++ code-chalenges-401/Singly-Linked-Lists/readme.md
@@ -1,8 +1,22 @@
-# linked-list-kth
+# linked-list
+
+##  link-list-kth
 i need to add a methode to my Linked list that will
 have k  number as a parameter and Return the node’s
  value that is k places from the tail of the linked list.
 and for wrong input return Exception
-# whiteboared 
+
+### whiteboared
 
 ![whiteboared](linked-list-kth.jpg)
+
+----
+##  link-list-zip
+create a function that take tow linked lists as an argument
+and return a linked list contain the tow linked list where
+each node of the tow lists is next to the one across it on
+the other node.
+
+### whiteboared link-list-zip
+
+![whiteboared](/home/ahmad/301/data-structures-and-algorithms/code-chalenges-401/Singly-Linked-Lists/linked-list-zip.jpg)
