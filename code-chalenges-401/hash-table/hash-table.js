7b2e22bdc8ca7edd734062fea394927246c2eb98
ahmadhamzh <ahmadhamzh@ymail.com>
2021-12-09 17:28:23 +0200

    code 30

---
+++ code-chalenges-401/hash-table/hash-table.js
@@ -0,0 +1,50 @@
+
+const Linkedlist = require('../Singly-Linked-Lists/lib/Linkedlist')
+
+class Hashtable {
+
+    constructor(size) {
+        this.size = size,
+        this.arr = new Array(size)
+    }
+
+    hash(key) {
+
+        const hashedKey = key.split('').reduce((p, n) => p + n.charCodeAt(0), 0);
+        const indexValue = ((hashedKey * 525) % this.size)
+        return indexValue
+
+    }
+
+    add(key, value) {
+
+        const payLoad = { [key]: value };
+        const hashedKey = this.hash(key)
+        if (!this.arr[hashedKey]) {
+            this.arr[hashedKey] = new Linkedlist()
+        }
+        this.arr[hashedKey].append(payLoad)
+    }
+
+    contains(key) {
+        const hashedKey = this.hash(key)
+        if (this.arr[hashedKey]) {
+            return this.arr[hashedKey].includingObject(key)
+        }
+        return false
+    }
+
+    get(key) {
+        const hashedKey = this.hash(key)
+        if (this.arr[hashedKey]) {
+            return this.arr[hashedKey].getDataObject(key)
+        }
+
+    }
+
+
+}
+
+module.exports = Hashtable
+
+
